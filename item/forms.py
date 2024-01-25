739938ae66e215143869af4970b3fd6f54981025
zeindevs <zeindevs@gmail.com>
2024-01-25 13:28:04 +0700

    add edit item

--- item/forms.py
+++ item/forms.py
@@ -44,3 +44,38 @@ class NewItemForm(forms.ModelForm):
                 }
             ),
         }
+
+
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = (
+            "name",
+            "description",
+            "price",
+            "image",
+            "is_sold",
+        )
+
+        widgets = {
+            "name": forms.TextInput(
+                attrs={
+                    "class": INPUT_CLASSES,
+                }
+            ),
+            "description": forms.Textarea(
+                attrs={
+                    "class": INPUT_CLASSES,
+                }
+            ),
+            "price": forms.TextInput(
+                attrs={
+                    "class": INPUT_CLASSES,
+                }
+            ),
+            "image": forms.FileInput(
+                attrs={
+                    "class": INPUT_CLASSES,
+                }
+            ),
+        }
--- item/views.py
+++ item/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 
-from .forms import NewItemForm
+from .forms import EditItemForm, NewItemForm
 from .models import Item
 
 
@@ -31,7 +31,27 @@ def new(request):
     else:
         form = NewItemForm()
 
-    data = {"form": form}
+    data = {"form": form, "title": "New item"}
+
+    return render(request, "item/form.html", data)
+
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == "POST":
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            item.save()
+
+            return redirect("item:detail", pk=item.id)
+
+    else:
+        form = EditItemForm(instance=item)
+
+    data = {"form": form, "title": "Edit item"}
 
     return render(request, "item/form.html", data)
 
