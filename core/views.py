20810c90c8bcb39c4a729f7e655dfe4e9f7bf4cf
zeindevs <zeindevs@gmail.com>
2024-01-25 11:27:49 +0700

    add item, category

--- core/views.py
+++ core/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
+from item.models import Category, Item
+
 
 def index(request):
-    return render(request, "core/index.html")
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+
+    data = {"categories": categories, "items": items}
+
+    return render(request, "core/index.html", data)
 
 
 def contact(request):
